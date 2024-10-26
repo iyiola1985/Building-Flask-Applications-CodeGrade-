04a4060596436966e358adc893b55af96a63addb
iyiola olusola ebenezer <sholaiyiola1966@gmail.com>
2024-10-26 12:17:55 +1100

    first commit

--- server/app.py
+++ server/app.py
@@ -1,5 +1,14 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+@app.route('/<username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
